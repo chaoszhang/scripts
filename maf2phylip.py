6152ef71b0477a7f9d90d1d3dd522fabd9a368df
Chao Zhang <chaozhang@berkeley.edu>
2025-06-27 13:40:35 +0200

    Update maf2phylip.py

--- maf2phylip.py
+++ maf2phylip.py
@@ -1,5 +1,10 @@
 import sys
 
+def printAlignment(seqs):
+    print(len(alignment), len(alignment[0][1]))
+    for e in alignment:
+        print(e[0], e[1])
+
 alignment = []
 for line in open(sys.argv[1]):
     line = line.split()
@@ -7,9 +12,8 @@ for line in open(sys.argv[1]):
         continue
     if line[0][0] == "a":
         if len(alignment) >= 4:
-            print(len(alignment), len(alignment[0][1]))
-            for e in alignment:
-                print(e[0], e[1])
+            printAlignment(alignment)
         alignment = []
-    elif int(line[3]) > 100:
+    elif int(line[3]) > 100 and (len(line[1]) < 3 || line[1][0:3] != "Anc") :
         alignment.append((line[1], line[6]))
+printAlignment(alignment)
